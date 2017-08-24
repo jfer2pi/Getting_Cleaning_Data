0ab9e4db9b5c38538659f27559d217730b996ac4
Fernando Rodriguez Alvarado <fernandorodrig@gmail.com>
2017-08-24 06:31:02 -0500

    Working

--- week_2/week2_quiz.R
+++ week_2/week2_quiz.R
@@ -1,10 +1,35 @@
 # Getting and Cleaning Data, Week 2 Quiz
 
+setwd("/Users/fer/Dropbox/Coursera DS/Getting_Cleaning_Data/week_2")
+
 # Question 1:  Register an application with the Github API here https://github.com/settings/applications.
 # Access the API to get information on your instructors repositories (hint: this is the url you want
 # "https://api.github.com/users/jtleek/repos"). Use this data to find the time that the datasharing
 # repo was created.  What time was it created?
 
+install.packages("jsonlite")
+library("httr",jsonlite)
+oauth_endpoints("github")
+
+myapp <- oauth_app("jfertest", 
+                   key = "aa1069dc69d329d8b7fe", 
+                   secret = "cdb338543b67c9da798e946a3fe631a644668c43")
+
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+gtoken <- config(token = github_token)
+
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+
+stop_for_status(req)
+content(req)
+
+json1 <- content(req)
+jsonData <- jsonlite::fromJSON("https://api.github.com/users/jtleek/repos")
+
+created_on <- cbind(jsonData$name, jsonData$created_at)
+colnames(created_on) <- c("repo", "created on")
+datasharing_created_on <- subset(created_on, created_on[, 1] == "datasharing")
 
 
 # Question 2:  The sqldf package allows for execution of SQL commands on R 
@@ -34,7 +59,7 @@ acs <- read.csv("acs.csv", header = T)
 library("XML")
 
 url4 <- "http://biostat.jhsph.edu/~jleek/contact.html"
-contact <- download.file(url4, "contact.hmtl", method = "curl")
+contact <- download.file(url4, "contact.html", method = "curl")
 
 leekparse <- readLines("contact.html")
 
@@ -53,8 +78,8 @@ file5 <- download.file(url5, "file5.for", method = "curl")
 install.packages("foreign")
 library("foreign")
 
-noaa <- read.fwf("noaa.for.for", widths = 12, sep = " " header = T)
-noaa
+noaa <- read.fwf("file5.for", widths = c(10, 3, 5, 5, 5, 5, 5, 5, 5), skip = 3, sep = "\t", header)
+noaa[, -c(2)]
 
 
 
